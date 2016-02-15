dc6285443bcd14d657edc561a486b62d43626498
dodgervl <dodgervl@yandex.ru>
2016-02-15 21:04:26 +0300

    Loading animation added; freeze at opening fixed

--- app/src/main/java/org/pg/telegramchallenge/EmptyFragment.java
+++ app/src/main/java/org/pg/telegramchallenge/EmptyFragment.java
@@ -3,12 +3,15 @@ package org.pg.telegramchallenge;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,6 +20,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import org.pg.telegramchallenge.Adapters.BottomSheetAdapter;
 import org.pg.telegramchallenge.views.BaseChatItemView;
@@ -27,11 +31,14 @@ import org.pg.telegramchallenge.views.ChatListItemView;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EmptyFragment extends Fragment implements View.OnClickListener {
+public class EmptyFragment extends Fragment implements View.OnClickListener, LoaderManager.LoaderCallbacks<Cursor> {
 
 
     private LinearLayoutManager mLayoutManager;
     private BottomSheetAdapter mAdapter;
+
+    CursorLoader cursorLoader; // for order
+    Cursor cursor;
 
     public EmptyFragment() {
         // Required empty public constructor
@@ -87,45 +94,97 @@ public class EmptyFragment extends Fragment implements View.OnClickListener {
             }
         });
 
+//        cursorLoader = new CursorLoader(
+//                getContext(),
+//                sourceUri,
+//                null,
+//                null,
+//                null,
+//                MediaStore.Images.Media.DATE_TAKEN + " DESC");
+
         return view;
     }
 
     final Uri sourceUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
+    private RecyclerView mBottomSheetRecyclerView;
+    private ProgressBar mBottomSheetProgressBar;
+
     @Override
     public void onClick(View v) {
 
         Context context = getContext();
-        CursorLoader cursorLoader = new CursorLoader(
-                context,
-                sourceUri,
-                null,
-                null,
-                null,
-                MediaStore.Images.Media.DATE_TAKEN + " DESC"); // for order
-
-        Cursor cursor = cursorLoader.loadInBackground();
 
         mBottomSheet = getLayoutInflater(null).inflate(R.layout.media_bottom_sheet, null);
 
-        mTakePhotoTextView = mBottomSheet.findViewById(R.id.take_photo_text_view);
-        mFromGalleryTextView = mBottomSheet.findViewById(R.id.from_gallery_text_view);
+        mBottomSheetProgressBar = (ProgressBar) mBottomSheet.findViewById(R.id.progress_bar_bottom_sheet);
 
-        RecyclerView recyclerView = (RecyclerView) mBottomSheet.findViewById(R.id.bottom_sheet_recycler);
+        mBottomSheetRecyclerView = (RecyclerView) mBottomSheet.findViewById(R.id.bottom_sheet_recycler);
         mLayoutManager = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
-//        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(mLayoutManager);
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mBottomSheetRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new BottomSheetAdapter(context, cursor);
-        recyclerView.setAdapter(mAdapter);
+        getLoaderManager().initLoader(0, null, this);
+//        cursor = cursorLoader.loadInBackground();
+
+//        mAdapter = new BottomSheetAdapter(context, cursor);
+//        mBottomSheetRecyclerView.setAdapter(mAdapter);
 
         final Dialog mBottomSheetDialog = new Dialog (context,
                 R.style.MaterialDialogSheet);
+
+        DialogListener listener = new DialogListener();
+        mBottomSheetDialog.setOnCancelListener(listener);
+        mBottomSheetDialog.setOnDismissListener(listener);
+
         mBottomSheetDialog.setContentView (mBottomSheet);
         mBottomSheetDialog.setCancelable (true);
         mBottomSheetDialog.getWindow ().setLayout (LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         mBottomSheetDialog.getWindow ().setGravity (Gravity.BOTTOM);
         mBottomSheetDialog.show ();
+    }
+
+    class DialogListener implements DialogInterface.OnCancelListener, DialogInterface.OnDismissListener{
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            mBottomSheetRecyclerView = null;
+            mAdapter = null;
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            mBottomSheetRecyclerView = null;
+            mAdapter = null;
+        }
+    }
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        Toast.makeText(getContext(), "onCreateLoader", Toast.LENGTH_SHORT).show();
+
+        return new CursorLoader(
+                getContext(),
+                sourceUri,
+                null,
+                null,
+                null,
+                MediaStore.Images.Media.DATE_TAKEN + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        Toast.makeText(getContext(), "onLoadFinished", Toast.LENGTH_SHORT).show();
+        if (mBottomSheetRecyclerView!=null) {
+            mBottomSheetProgressBar.setVisibility(View.GONE);
+            mBottomSheetRecyclerView.setVisibility(View.VISIBLE);
+            mAdapter = new BottomSheetAdapter(getContext(), cursor);
+            mBottomSheetRecyclerView.setAdapter(mAdapter);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 }
